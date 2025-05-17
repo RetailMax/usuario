9f3527d4438ac051d218551b55c1b23a584c309f
KhanIvall <ar.arayao@duocuc.cl>
2025-05-16 22:10:47 -0400

    Creación de clases en model e implementación de herencia en clase usuario (#2)

--- src/main/java/cap/usuario/model/Ciudad.java
+++ src/main/java/cap/usuario/model/Ciudad.java
@@ -1,6 +1,5 @@
 package cap.usuario.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,17 +10,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "region")
+@Table(name = "ciudad")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Region {
+public class Ciudad {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idRegion;
-
-    @Column(nullable = false)
-    private String nombre;
+    private Integer idCiudad;
 
 }
---
+++ src/main/java/cap/usuario/model/Comprador.java
@@ -0,0 +1,45 @@
+package cap.usuario.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "comprador")
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comprador extends Usuario{
+
+    @Column(nullable=true)
+    private Integer telefono;
+
+    @Column(nullable=false)
+    private String direccion;
+
+    @OneToOne
+    @JoinColumn(nullable=false)
+    private Region region;
+
+    @OneToOne
+    @JoinColumn(nullable=false)
+    private Ciudad ciudad;
+
+    @OneToOne
+    @JoinColumn(nullable=false)
+    private Comuna comuna;
+
+    @Column(nullable=false)
+    private Integer codigoPostal;
+
+    @Column(nullable=false)
+    private String correoElectronico;
+
+}
