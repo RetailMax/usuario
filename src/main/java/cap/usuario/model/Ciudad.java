7687e2bec3b7ade5d43cf87dc073fdd906b0f3f3
Catalinapalma <cata.palmam@duocuc.cl>
2025-05-23 21:31:11 -0400

    Modificaciondeclases (#6)
    * RepositoryInterface
    
    * Agrego modificacionclases

--- src/main/java/cap/usuario/model/Ciudad.java
+++ src/main/java/cap/usuario/model/Ciudad.java
@@ -1,13 +1,7 @@
 package cap.usuario.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "ciudad")
