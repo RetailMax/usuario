429db1353a2e5106e57c4ab6bc6c015781328f47
Catalinapalma <cata.palmam@duocuc.cl>
2025-05-26 17:18:34 -0400

    Agrego configuración de base de datos (#8)

--- src/main/java/cap/usuario/controller/UsuarioController.java
+++ src/main/java/cap/usuario/controller/UsuarioController.java
@@ -3,9 +3,7 @@ package cap.usuario.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import cap.usuario.model.Usuario;
 import cap.usuario.service.UsuarioService;
@@ -20,6 +18,10 @@ public class UsuarioController {
     @GetMapping
     public List<Usuario> mostrarUsuarios(){
         return usuarioService.findAll();
-    } 
+    }
 
+    @PostMapping
+    public Usuario crearUsuario(@RequestBody Usuario nuevoUsuario) {
+        return usuarioService.save(nuevoUsuario);
+    }
 }
